b98d329618452b74a747f62a3029eb651df956ac
chmadran <charlotte.madrangeas@gmail.com>
2024-11-14 21:44:03 +0100

    Stop tracking _build folder

--- ft_ality/_build/default/src/parser/parser.mli
+++
@@ -1,9 +0,0 @@
-
-type key_mapping = { key : string; action : string }
-type move = { name : string; key_combination : string list }
-
-val parse_grammar_file : string -> key_mapping list * move list
-val show_key_mappings : key_mapping list -> unit
-val show_move_sequences : move list -> unit
-val process_grammar_file : string -> string list * string list
-
